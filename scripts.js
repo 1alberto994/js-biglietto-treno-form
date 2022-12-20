f3301cf29f416a4b2e820990669b802641a0c249
1alberto994 <alby94ct@gmail.com>
2022-12-20 17:50:18 +0100

    esercizio completato

--- scripts.js
+++ scripts.js
@@ -1,48 +1,48 @@
-const prezzoAkm=0.21;
-const ageUno=18;
-const ageDue=65
-// km =km.isNaN(km);
-// age = km.isNaN(age)
-// const prezzoBiglietto=prezzoAkm*km;
-// console.log("il prezzo pieno è di:",prezzoBiglietto);
-// if(km==true || age==true){
-//     console.log("non valido ti prego di aggiornare la pagina");
 const kmInput=document.getElementById("km");
 const etaInput=document.getElementById("eta");
-const buttonGenera=document.getElementById("genera")
+const buttonGenera=document.getElementById("genera");
+const nameInput=document.getElementById("name-input");
 buttonGenera.addEventListener('click',
     function(){
-        if(etaInput=="minorenne"){
-            const prezzoBigliettoMinorenni= Math.floor(prezzoBiglietto*20/100);
-            console.log( "sconto 20%:",prezzoBigliettoMinorenni);
-            const prezzoScontato=prezzoBiglietto-prezzoBigliettoMinorenni;
-            console.log("lo sconto è di:", prezzoScontato);
-            const finalPrice = prezzoScontato.toFixed(2);
-            
-        }
-        else if(etaInput=="over65"){
-            const prezzoBigliettoOver =Math.floor(prezzoBiglietto*40/100);
-            console.log( "sconto 40%:",prezzoBigliettoOver);
-            const bigliettoScontatoOver=prezzoBiglietto-prezzoBigliettoOver;
-            console.log("lo sconto è di:",bigliettoScontatoOver);
-            const finalPriceOver=bigliettoScontatoOver.toFixed(2);
-            alert("il prezzo finale è di€:",finalPriceOver);
+            const namVal=nameInput.value;
+            const knVal = kmInput.value;
+            const etaVal = etaInput.value;
+            const prezzoAkm = 0.21;
+            const prezzoBiglietto= prezzoAkm * knVal
+            console.log('km input: '+ knVal);
+            console.log('il prezzo è: '+prezzoBiglietto);
+            document.getElementById("second-title").innerHTML="il prezzo del biglietto";
+            if(etaVal=="minorenne"){
+                console.log(prezzoBiglietto)
+                const prezzoBigliettoMinorenni= prezzoBiglietto/100*20;
+                console.log( "sconto 20%:",prezzoBigliettoMinorenni);
+                const prezzoScontato=prezzoBiglietto-prezzoBigliettoMinorenni;
+                console.log("lo sconto è di:", prezzoScontato);
+                prezzoScontato.toFixed(2);
+                document.getElementById("nome-utente").innerHTML= "il nome del passeggero è :"+ namVal
+                document.getElementById('eta-utente').innerHTML = "l'età del passeggero è: "+etaVal;
+                document.getElementById("prezzo-utente").innerHTML="il prezzo è di:"+ prezzoScontato
+                
+            }
+            else if(etaVal=="over65"){
+                
+                console.log(prezzoBiglietto)
+                const prezzoBigliettoOver =(prezzoBiglietto/100*40);
+                console.log( "sconto 40%:",prezzoBigliettoOver);
+                const bigliettoScontatoOver=prezzoBiglietto-prezzoBigliettoOver;
+                console.log("lo sconto è di:",bigliettoScontatoOver);
+                bigliettoScontatoOver.toFixed(2);
+                document.getElementById("nome-utente").innerHTML= "il nome del passeggero è :"+ namVal
+                document.getElementById('eta-utente').innerHTML = "l'età del passeggero è: "+etaVal;
+                document.getElementById("prezzo-utente").innerHTML="il prezzo è di:"+ bigliettoScontatoOver
             }    
-        
-        else if(etaInput=="maggiorenne") {
             
-             prezzoBiglietto.toFixed(2);
-             alert("il prezzo finale è di€:",prezzoBiglietto.toFixed(2));
-        }
-        
-        
+            else if(etaVal=="maggiorenne") {
+                document.getElementById("nome-utente").innerHTML= "il nome del passeggero è :"+ namVal
+                document.getElementById('eta-utente').innerHTML = "l'età del passeggero è: "+etaVal;
+                document.getElementById("prezzo-utente").innerHTML="il prezzo è di:"+ prezzoBiglietto
+                prezzoBiglietto.toFixed(2);
+            }
+           
     }
 )
-
-   
-
-
-
-
-
-
